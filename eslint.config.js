d294dabfcf95cb729321fbde8b141aeabd1152d4
Min-Thein-Khaing <nosoonerthan1@gmail.com>
2026-09-06 20:28:16 +0700

    eslint setup

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import eslintPluginAstro from "eslint-plugin-astro"
+
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    rules:{}
+  },
+]
